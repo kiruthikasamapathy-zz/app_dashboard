494fdaf00e493997b1055242d2587ca82484875c
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-07-10 17:00:53 +1000

    Highlight the version if not matching with prod version

--- app/js/controllers/status_controller.js
+++ app/js/controllers/status_controller.js
@@ -1,8 +1,8 @@
-angular.module("app").controller("StatusController", function($scope, $http, StatusResource,VERSION_CONFIG_FILE) {
+angular.module("app").controller("StatusController", function($scope, $http, $filter, StatusResource, VERSION_CONFIG_FILE) {
   //$scope.status_from_service = StatusResource.get();
 
-  $http.get(VERSION_CONFIG_FILE).success(function (data){
-      $scope.status = data;
+  $http.get(VERSION_CONFIG_FILE).success(function(data) {
+    $scope.status = data;
   });
 
   $scope.predicate = 'id';
@@ -11,5 +11,19 @@ angular.module("app").controller("StatusController", function($scope, $http, Sta
     $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
     $scope.predicate = predicate;
   };
+
+  $scope.set_color = function(all_versions, current_version) {
+
+    all_env_versions = angular.fromJson(all_versions);
+    var prod_version = $filter('filter')(all_env_versions, {
+      name: 'PROD'
+    })[0].version;
+
+    if (current_version != prod_version) {
+      return {
+        color: "red"
+      }
+    }
+  }
 
 });
