c75037c366161a38594c27091bd9999372a83d8a
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-07-14 16:40:09 +1000

    add more e2e tests to check if environments are populated

--- spec-e2e/status_spec.js
+++ spec-e2e/status_spec.js
@@ -3,11 +3,17 @@ describe('application dashboard app', function () {
   describe('visiting the applications status', function () {
     beforeEach(function () {
       browser.get('/status');
-      appRows = element.all(by.repeater('application in status.applications'));
+      appData = element.all(by.repeater('application in status.applications'));
+      environmentsData = element.all(by.repeater('env in environments_config.environments'));
     });
 
     it('should show me a list of apps', function() {
-      expect(appRows.count()).toEqual(11);
+      expect(appData.count()).toEqual(11);
     });
+
+    it('should show me all enviornments in the header', function() {
+      expect(environmentsData.count()).toEqual(6);
+    });
+
   });
 });
