a61dc7f4ab9e0a4e5fad64b78ae186d710f20e9f
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-07-16 14:52:22 +1000

    adding more unit test cases for status controller

--- spec/services/env_data_parser_spec.js
+++ spec/services/env_data_parser_spec.js
@@ -9,12 +9,44 @@ describe('Factory: envDataParserService', function() {
 
   it('should return prod version', function() {
     var prod_version = 1.2;
-    var all_versions = [ { name : "PROD", "version" : prod_version } ];
+    var all_versions = [{
+      name: "PROD",
+      version: prod_version
+    }];
     expect(service.get_prod_version(all_versions)).toBe(prod_version);
   });
 
   it('should return empty if prod version is not available', function() {
-    var all_versions = [ { name : "PRO", "version" : "1.21" } ];
+    var all_versions = [{
+      name: "PRO",
+      version: 1.21
+    }];
+    expect(service.get_prod_version(all_versions)).toBe("");
+  });
+
+  it('should return empty if input is not well formed', function() {
+    var all_versions = [{
+      noname: "PROD",
+      version: 1.21,
+    }];
+    expect(service.get_prod_version(all_versions)).toBe("");
+  });
+
+  it('should return empty if input is not of expected format', function() {
+    var all_versions = [{
+      noname: "PROD",
+      version: 1.21
+    }];
+    expect(service.get_prod_version(all_versions)).toBe("");
+  });
+
+  it('should return empty if input is empty', function() {
+    var all_versions = [];
+    expect(service.get_prod_version(all_versions)).toBe("");
+  });
+
+  it('should return empty if input is null', function() {
+    var all_versions = null;
     expect(service.get_prod_version(all_versions)).toBe("");
   });
 });
