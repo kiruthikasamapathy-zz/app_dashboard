37a9c1c18f8901e0f64d0d31e6d02dcc3817b4e2
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-07-16 12:19:20 +1000

    Handle scenario when prod version is not available

--- app/js/services/env_data_parser.js
+++ app/js/services/env_data_parser.js
@@ -1,13 +1,11 @@
 angular.module("app").factory('envDataParserService', function($filter) {
   var envDataParserService = {
     get_prod_version: function(all_versions) {
-      all_env_versions = angular.fromJson(all_versions);
-      //console.log("all versions " + all_env_versions);
-      //console.log("sec" + angular.fromJson(all_env_versions));
-      var prod_version = $filter('filter')(all_env_versions, {
-        name: 'PROD'
-      })[0].version;
-      return prod_version;
+      all_env = angular.fromJson(all_versions);
+      prod_env = $filter('filter')(all_env, {
+        name: "PROD"
+      });
+      return prod_env.length > 0 ? prod_env[0].version : "";
     }
   };
   return envDataParserService;
--- spec/services/env_data_parser_spec.js
+++ spec/services/env_data_parser_spec.js
@@ -8,7 +8,13 @@ describe('Factory: envDataParserService', function() {
   }))
 
   it('should return prod version', function() {
-    var all_versions = { "name" : "PROD", "version" : 1.2 };
-    expect(service.get_prod_version(all_versions)).toBe("test");
+    var prod_version = 1.2;
+    var all_versions = [ { name : "PROD", "version" : prod_version } ];
+    expect(service.get_prod_version(all_versions)).toBe(prod_version);
+  });
+
+  it('should return empty if prod version is not available', function() {
+    var all_versions = [ { name : "PRO", "version" : "1.21" } ];
+    expect(service.get_prod_version(all_versions)).toBe("");
   });
 });
