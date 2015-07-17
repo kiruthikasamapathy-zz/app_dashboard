62715f8cefd9a2f8d424fcf0b7d5fb5dbe5cdc13
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-07-17 11:34:02 +1000

    Include dependencies and js files for multiselect dropdown

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-angular.module("app", ["ngResource", "ngRoute"]).run(function($rootScope) {
+angular.module("app", ["ngResource", "ngRoute", "angularjs-dropdown-multiselect"]).run(function($rootScope) {
   // adds some basic utilities to the $rootScope for debugging purposes
   $rootScope.log = function(thing) {
     console.log(thing);
