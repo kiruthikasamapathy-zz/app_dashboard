94cf7f1a14928c4dddce6ced7bdc8ce9f5e15cae
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-07-21 12:29:38 +1000

    Using ngDialog to do the pop up - for showing date and sub components

--- app/js/controllers/status_controller.js
+++ app/js/controllers/status_controller.js
@@ -1,44 +1,50 @@
-angular.module("app").controller("StatusController", function($scope, statusService, envDataParserService, appDataParserService) {
+angular.module("app").controller("StatusController", function($scope, ngDialog, statusService, envDataParserService, appDataParserService) {
 
-  $scope.selectedAppsModel = [];
-  statusService.get_all().then(function(data) {
-    $scope.status = data;
-    $scope.selectedAppsModel = angular.copy(data.applications);
+ $scope.selectedAppsModel = [];
+ statusService.get_all().then(function(data) {
+  $scope.status = data;
+  $scope.selectedAppsModel = angular.copy(data.applications);
+ });
+
+ $scope.predicate = 'id';
+ $scope.reverse = false;
+
+ $scope.is_app_hidden = function(id) {
+  if (appDataParserService.contains($scope.selectedAppsModel, id)) {
+   return false;
+  }
+  return true;
+ };
+ $scope.app_dropdown_settings = {
+  enableSearch: true,
+  externalIdProp: ''
+ };
+ $scope.app_dropdown_customtexts = {
+  buttonDefaultText: 'Select Apps',
+  dynamicButtonTextSuffix: 'Apps',
+  checkAll: 'Show All',
+  uncheckAll: 'Hide All',
+  searchPlaceholder: 'Select Apps',
+ };
+
+ $scope.order = function(predicate) {
+  $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+  $scope.predicate = predicate;
+ };
+
+ $scope.highlight_prod_differences = function(all_versions, current_version) {
+  var prod_version = envDataParserService.get_prod_version(all_versions);
+  if (current_version < prod_version) {
+   return {
+    color: "red"
+   };
+  }
+ };
+
+ $scope.clickToOpen = function() {
+  ngDialog.open({
+   template: 'subcomponents.html'
   });
-
-  $scope.predicate = 'id';
-  $scope.reverse = false;
-
-  $scope.is_app_hidden = function(id) {
-    if (appDataParserService.contains($scope.selectedAppsModel, id)) {
-      return false;
-    }
-    return true;
-  };
-  $scope.app_dropdown_settings = {
-    enableSearch: true,
-    externalIdProp: ''
-  };
-  $scope.app_dropdown_customtexts = {
-    buttonDefaultText: 'Select Apps',
-    dynamicButtonTextSuffix: 'Apps',
-    checkAll: 'Show All',
-    uncheckAll: 'Hide All',
-    searchPlaceholder: 'Select Apps',
-  };
-
-  $scope.order = function(predicate) {
-    $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
-    $scope.predicate = predicate;
-  };
-
-  $scope.highlight_prod_differences = function(all_versions, current_version) {
-    var prod_version = envDataParserService.get_prod_version(all_versions);
-    if (current_version < prod_version) {
-      return {
-        color: "red"
-      };
-    }
-  };
+ };
 
 });
